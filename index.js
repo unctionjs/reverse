d8fe4564d2533707ddc885b4d767398bdea25fc5
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2018-05-26 20:17:30 -0700

    Removing packages we don't need

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-import reduceValues from "@unction/reducevalues"
-import flip from "@unction/flip"
-import fresh from "@unction/fresh"
-import prepend from "@unction/prepend"
-
-export default function reverse (orderedList: ArrayType | string): ArrayType | string {
-  return reduceValues(
-    flip(prepend)
-  )(
-    fresh(orderedList)
-  )(
-    orderedList
-  )
+import reduceValues from "@unction/reducevalues";
+import flip from "@unction/flip";
+import fresh from "@unction/fresh";
+import prepend from "@unction/prepend";
+export default function reverse(orderedList) {
+  return reduceValues(flip(prepend))(fresh(orderedList))(orderedList);
 }
