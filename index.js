f963ef67072b16e42b6db94cce1d468e7275b8ea
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-04-13 21:01:45 -0700

    Autolinting

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import reduceValues from "@unction/reducevalues";
 import flip from "@unction/flip";
 import fresh from "@unction/fresh";
 import prepend from "@unction/prepend";
-export default function reverse(orderedList) {
+export default function reverse (orderedList) {
   return reduceValues(flip(prepend))(fresh(orderedList))(orderedList);
 }
--- test.js
+++ test.js
@@ -1,31 +1,31 @@
 /* eslint-disable no-magic-numbers */
-import {test} from "tap"
+import {test} from "tap";
 
-import reverse from "./index"
+import reverse from "./index";
 
 test(({same, end}) => {
   same(
     reverse([1, 2, 3]),
     [3, 2, 1]
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 test(({same, end}) => {
   same(
     reverse([1]),
     [1]
-  )
+  );
 
-  end()
-})
+  end();
+});
 
 test(({same, end}) => {
   same(
     reverse("abc"),
     "cba"
-  )
+  );
 
-  end()
-})
+  end();
+});
