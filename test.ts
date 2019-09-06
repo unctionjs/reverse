5518f920564c63684791c1526f44d2a663bb7500
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-06 14:56:51 -0700

    The transformer didnt fix this

--- test.ts
+++ test.ts
@@ -1,14 +1,14 @@
 /* eslint-disable no-magic-numbers */
 import reverse from "./index";
 
-test(() => {
+test("works", () => {
   expect(reverse([1, 2, 3])).toEqual([3, 2, 1]);
 });
 
-test(() => {
+test("works", () => {
   expect(reverse([1])).toEqual([1]);
 });
 
-test(() => {
+test("works", () => {
   expect(reverse("abc")).toEqual("cba");
 });
