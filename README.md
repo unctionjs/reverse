23d04c37387e6103734f952a8dd8f46dbb4725ee
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2020-11-29 09:49:51 -0800

    Removing more bad types

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 ![Stability][BADGE_STABILITY]
 ![Dependencies][BADGE_DEPENDENCY]
 
-> OrderedArray<A> | Set<A> | RecordType<unknown, A> | string => OrderedArray<A> | Set<A> | RecordType<unknown, A> | string
+> OrderedArray<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string => OrderedArray<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string
 
 Takes an ordered enumerable type and returns the reverse version of it.
 
--- index.ts
+++ index.ts
@@ -3,6 +3,6 @@ import flip from "@unction/flip";
 import fresh from "@unction/fresh";
 import prepend from "@unction/prepend";
 
-export default function reverse<A> (orderedList: OrderedArray<A> | Set<A> | RecordType<unknown, A> | string): OrderedArray<A> | Set<A> | RecordType<unknown, A> | string {
+export default function reverse<A> (orderedList: OrderedArray<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string): OrderedArray<A> | Set<A> | Record<string | number | symbol, B> | Map<B, A> | string {
   return reduceValues(flip(prepend))(fresh(orderedList))(orderedList);
 }
